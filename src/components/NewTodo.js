472887d27c700dcfa1e12a27acda3857a5d5ad9b
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-07-01 14:49:35 -0500

    completed for now working version of to do list. add useState to NewTodo.js. added text changehandler. updated value of input to equal the current entered state.

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import './NewTodo.css'
+import React, { useState } from 'react';
+import './NewTodo.css';
 
 const NewTodo = props => {
-  const newTodoHandler = event => {
-     event.preventDefault()
-     props.addNewTodo(event)
+  const [enteredText, setEnteredText] = useState('')
+  const newTodoHandler = event => { 
+    event.preventDefault();
+     
+    const todo = {
+        id: Math.random.toString(),
+        text: enteredText
+    };
+
+    setEnteredText('');
+
+    props.addNewTodo(todo);
   }  
+
+  const textChangeHandler = event => {
+     setEnteredText(event.target.value)
+  }
+
   
   return (
       <form className="new-todo" onSubmit={newTodoHandler}>
-          <input type="text" />
+          <input type="text" value={enteredText} onChange={textChangeHandler}/>
           <button type="submit">Submit</button>
       </form>
   )
