fd660ee9bc94b3f93e4a8387f5972763be8a019f
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-07-01 12:16:28 -0500

    created todoList component

---
+++ src/components/todoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <div className="to-do-list">
+        <h1>To Do</h1>
+     <ul className="to-do" >
+       <li>Something</li>
+     </ul>
+   </div>
+    )
+}
+export default TodoList;
